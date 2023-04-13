a07905c311518ecb1848bb74c25670958b889155
vijay-ram365 <ramkissoon.vijay@gmail.com>
2023-04-13 10:50:25 -0400

    Deploy React App to GitHub Pages

---
+++ precache-manifest.24bee5b95cecd50ad7a3fff2bcb0f84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed4bff48a7db7a65953b",
+    "url": "/Star-Wars-API-React/static/css/main.eb9e01c3.chunk.css"
+  },
+  {
+    "revision": "ed4bff48a7db7a65953b",
+    "url": "/Star-Wars-API-React/static/js/main.330aff9b.chunk.js"
+  },
+  {
+    "revision": "f064a410ba94ebeb6bc2",
+    "url": "/Star-Wars-API-React/static/js/runtime~main.a6d37812.js"
+  },
+  {
+    "revision": "4260ed07662bf2ae07d6",
+    "url": "/Star-Wars-API-React/static/js/2.a9d2c23b.chunk.js"
+  },
+  {
+    "revision": "0d6fdec441314230fab66ac02e9ad29d",
+    "url": "/Star-Wars-API-React/index.html"
+  }
+];
